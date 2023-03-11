dc2bd130975fb1b1deec6eab48058e245045e520
Dror Seltzer <drorsel@gmail.com>
2023-03-11 23:38:40 +0200

    attribute type safe

--- src/lib/double-u-c-generator.ts
+++ src/lib/double-u-c-generator.ts
@@ -158,23 +158,25 @@ export class DoubleUCGenerator {
       switch (attribute.type) {
         case 'number':
           getterString = `this.hasAttribute('${pascalToKebab(attribute.name)}') ? +this.getAttribute('${pascalToKebab(attribute.name)}') : undefined`;
-          setterString = `this.setAttribute('${pascalToKebab(attribute.name)}', value.toString());`;
+          setterString = `if (typeof value !== 'number') throw new TypeError('not a number');\n  this.setAttribute('${pascalToKebab(attribute.name)}', value.toString());`;
           break;
         case 'boolean':
           getterString = `this.hasAttribute('${pascalToKebab(attribute.name)}') ? this.getAttribute('${pascalToKebab(attribute.name)}') === 'true' : false`;
-          setterString = `this.setAttribute('${pascalToKebab(attribute.name)}', !!value ? 'true' : 'false');`;
+          setterString = `if (typeof value !== 'boolean') throw new TypeError('not a boolean');\n  this.setAttribute('${pascalToKebab(
+            attribute.name
+          )}', !!value ? 'true' : 'false');`;
           break;
         case 'array':
           getterString = `this.hasAttribute('${pascalToKebab(attribute.name)}') ? JSON.parse(this.getAttribute('${pascalToKebab(attribute.name)}')) : undefined`;
-          setterString = `this.setAttribute('${pascalToKebab(attribute.name)}', JSON.stringify(value));`;
+          setterString = `if (!Array.isArray(value)) throw new TypeError('not an array');\n  this.setAttribute('${pascalToKebab(attribute.name)}', JSON.stringify(value));`;
           break;
         case 'json':
           getterString = `this.hasAttribute('${pascalToKebab(attribute.name)}') ? JSON.parse(this.getAttribute('${pascalToKebab(attribute.name)}')) : undefined`;
-          setterString = `this.setAttribute('${pascalToKebab(attribute.name)}', JSON.stringify(value));`;
+          setterString = `if (typeof value !== 'string') throw new TypeError('not a json');\n  this.setAttribute('${pascalToKebab(attribute.name)}', JSON.stringify(value));`;
           break;
         default:
           getterString = `this.getAttribute('${pascalToKebab(attribute.name)}')`;
-          setterString = `this.setAttribute('${pascalToKebab(attribute.name)}', value);`;
+          setterString = `if (typeof value !== 'string') throw new TypeError('not a string');\n  this.setAttribute('${pascalToKebab(attribute.name)}', value);`;
       }
       attributesGettersString += `
         get ${attribute.name}() {
