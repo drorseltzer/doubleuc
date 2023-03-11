393a572ae2c3d98bbaf3a2bfe9679d6f8a6b1e9d
Dror Seltzer <drorsel@gmail.com>
2023-03-11 16:08:51 +0200

    git actions

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('jest').Config} */
-const config = {
-  reporters: ['default', ['jest-junit', { outputDirectory: 'reports', outputName: 'report.xml' }]]
-};
-
-module.exports = config;
